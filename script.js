ffebc91c757c31e95bfbcfc91fda2afaa08a4761
Stephen Lutz <60757563+stephenlutz@users.noreply.github.com>
2025-04-10 09:32:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+import GLea from 'https://unpkg.com/glea?module';
+
+const frag = document.getElementById('fragmentShader').textContent;
+const vert = document.getElementById('vertexShader').textContent;
+
+let texture = null;
+
+const glea = new GLea({
+  shaders: [
+    GLea.fragmentShader(frag),
+    GLea.vertexShader(vert)
+  ],
+  buffers: {
+    'pos': GLea.buffer(2, [1, 1,  -1, 1,  1,-1,  -1,-1])
+  }
+}).create();
+
+function loop(time) {
+  const { gl } = glea;
+  glea.clear();
+  glea.uni('width', glea.width);
+  glea.uni('height', glea.height);
+  glea.uni('time', time * .0002);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  requestAnimationFrame(loop);
+}
+
+function setup() {
+  const { gl } = glea;
+  window.addEventListener('resize', () => {
+    glea.resize();
+  });
+  loop(0);
+}
+
+setup();
